d344a67b2ac1d2bcd7d659f40c77d22ecc4637ab
George <tsintsadze7@gmail.com>
2023-04-20 16:34:47 +0400

    feat: add angular material

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from './material-design/material-design.module';
+import { StepperComponent } from './test/stepper/stepper.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './test/header/header.component';
+import { DatePickerComponent } from './test/date-picker/date-picker.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialDesignModule],
+  declarations: [AppComponent, StepperComponent, HeaderComponent, DatePickerComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialDesignModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- airways/src/app/material-design/material-design.module.ts
+++ airways/src/app/material-design/material-design.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatStepperModule} from '@angular/material/stepper'; 
-
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -12,6 +15,10 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatButtonModule, 
     MatToolbarModule,
     MatStepperModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
   ]
 })
 export class MaterialDesignModule { }
