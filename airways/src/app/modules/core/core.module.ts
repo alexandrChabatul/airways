d8995d5eb47ec1b8e54606e9d0c6afdc63d0dfc4
Nastya <himeriya97@mgmail.com>
2023-04-21 18:13:46 +0300

    feat: add base layout for header

--- airways/src/app/modules/core/core.module.ts
+++ airways/src/app/modules/core/core.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,11 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   exports: [
     HeaderComponent,
