c308691d86393d61a0733624949d5febe632c9a9
sashkill94 <sashkill94@gmail.com>
2023-04-21 17:55:59 +0300

    refactor: lint changes

--- airways/src/app/material-design/material-design.module.ts
+++ airways/src/app/material-design/material-design.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker'; 
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
-import {MatCardModule} from '@angular/material/card';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
   exports: [
-    MatButtonModule, 
+    MatButtonModule,
     MatToolbarModule,
     MatStepperModule,
     MatFormFieldModule,
@@ -31,6 +31,6 @@ import {MatDividerModule} from '@angular/material/divider';
     MatSlideToggleModule,
     MatIconModule,
     MatDividerModule,
-  ]
+  ],
 })
-export class MaterialDesignModule { }
+export class MaterialDesignModule {}
