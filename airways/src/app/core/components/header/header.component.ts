b3df8a61613d5b9ca93c10987ae13858fe2cdce6
Nastya <himeriya97@mgmail.com>
2023-04-24 17:27:51 +0300

    feat: add header steps and store

--- airways/src/app/core/components/header/header.component.ts
+++ airways/src/app/core/components/header/header.component.ts
@@ -1,31 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material/icon';
+import {Location} from '@angular/common';
+import { Store } from '@ngrx/store';
+import { selectCurrencyFormat, selectDateFormat } from '../../store/selectors/formats.selectors';
+import { changeDateFormat } from '../../store/actions/formats.actions';
+import { changeCurrencyFormat } from '../../store/actions/formats.actions';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'airways-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public dateFormats = [
-    {value: 1, viewValue: 'MM/DD/YYYY'},
-    {value: 2, viewValue: 'DD/MM/YYYY'},
-    {value: 3, viewValue: 'YYYY/DD/MM'},
-    {value: 4, viewValue: 'YYYY/MM/DD'},
+    {value: 'MM/dd/yyyy', viewValue: 'MM/DD/YYYY'},
+    {value: 'dd/MM/yyyy', viewValue: 'DD/MM/YYYY'},
+    {value: 'yyyy/dd/MM', viewValue: 'YYYY/DD/MM'},
+    {value: 'yyyy/MM/dd', viewValue: 'YYYY/MM/DD'},
   ];
 
   public currencyFormats = [
-    {value: 1, viewValue: 'EUR'},
-    {value: 2, viewValue: 'USD'},
-    {value: 3, viewValue: 'RUB'},
-    {value: 4, viewValue: 'PLN'},
+    {value: 'EUR', viewValue: 'EUR'},
+    {value: 'USD', viewValue: 'USD'},
+    {value: 'RUB', viewValue: 'RUB'},
+    {value: 'PLN', viewValue: 'PLN'},
   ];
 
-  public selectedDateFormat = this.dateFormats[0].value;
-  public selectedCurrencyFormat = this.currencyFormats[0].value;
+  public selectedDateFormat = '';
+  public selectedCurrencyFormat = '';
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  public isMainPage = false;
+  public isBookingPage = false;
+  public stepNumber = 1;
+
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private location: Location,
+    private store: Store,
+    ) {
     iconRegistry.addSvgIcon('basket', sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/basket.svg'));
+  }
+
+  public ngOnInit(): void {
+    this.location.onUrlChange((path) => {
+      this.switchPage(path);
+    });
+
+    this.store.select(selectDateFormat).subscribe((val) => {
+      this.selectedDateFormat = val;
+    })
+
+    this.store.select(selectCurrencyFormat).subscribe((val) => {
+      this.selectedCurrencyFormat = val;
+    })
+  }
+
+  private switchPage(newPath: string): void {
+    const pathArray = newPath.split('/');
+    const mainPath = pathArray[1];
+
+    if (mainPath ===''){
+      this.isMainPage = true;
+      this.isBookingPage = false;
+    } else if (mainPath === 'booking') {
+      this.isMainPage = false;
+      this.isBookingPage = true;
+
+      if (pathArray.length > 2){
+        const innerPath = pathArray[2];
+
+        if (innerPath === 'passengers') {
+          this.stepNumber = 2;
+        }
+
+        if (innerPath === 'summary') {
+          this.stepNumber = 3;
+        }
+      }
+    } else {
+      this.isMainPage = false;
+      this.isBookingPage = false;
+    }
+  }
+
+  public changeDateFormat(newValue: MatSelectChange): void {
+    const dateFormat = newValue.value;
+    this.store.dispatch(changeDateFormat({dateFormat}));
+  }
+
+  public changeCurrencyFormat(newValue: MatSelectChange): void {
+    const currencyFormat = newValue.value;
+    this.store.dispatch(changeCurrencyFormat({currencyFormat}));
   }
 }
---
+++ airways/src/app/core/store/action-types/formats.action-types.ts
@@ -0,0 +1,4 @@
+export enum FormatsActionTypes {
+  CHANGE_CURRENCY_FORMAT = '[Core] Change currency format',
+  CHANGE_DATE_FORMAT = '[Core] Change date format',
+}
