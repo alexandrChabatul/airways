7f68ac11b88714fd21a5b50b9f47d03facd2eced
George <tsintsadze7@gmail.com>
2023-04-26 21:19:50 +0400

    refactor

--- airways/src/app/lazy-loaded-modules/auth/pages/auth-page.component.ts
+++ airways/src/app/lazy-loaded-modules/auth/pages/auth-page.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'airways-auth-page',
   templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.scss'],
 })
 export class AuthPageComponent {}
