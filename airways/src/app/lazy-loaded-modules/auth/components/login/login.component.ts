1c0796c5d383c7d832e8902e8a08f0b631bb446c
George <tsintsadze7@gmail.com>
2023-04-26 23:43:00 +0400

    feat: add part of login content

--- airways/src/app/lazy-loaded-modules/auth/components/login/login.component.ts
+++ airways/src/app/lazy-loaded-modules/auth/components/login/login.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'airways-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+    this.addSvgIcon('google');
+    this.addSvgIcon('facebook');
+  }
+
+  addSvgIcon(iconName: string) {
+    this.iconRegistry.addSvgIcon(
+      iconName,
+      this.sanitizer.bypassSecurityTrustResourceUrl(`../../../../assets/icons/${iconName}.svg`),
+    );
+  }
+
+  onGoogleSignIn() {}
+}
