d8e0fada8a92dab4edce6170b260c3b1c08f0ad8
Nastya <himeriya97@mgmail.com>
2023-04-27 20:02:55 +0300

    feat: add edit component

--- airways/src/app/lazy-loaded-modules/booking/booking.module.ts
+++ airways/src/app/lazy-loaded-modules/booking/booking.module.ts
@@ -4,9 +4,16 @@ import { FlightsPageComponent } from './pages/flights-page/flights-page.componen
 import { PassengersPageComponent } from './pages/passengers-page/passengers-page.component';
 import { SummaryPageComponent } from './pages/summary-page/summary-page.component';
 import { BookingRoutingModule } from './booking-routing.module';
+import { EditFlightComponent } from './components/edit-flight/edit-flight.component';
+import { MaterialDesignModule } from '../../material-design/material-design.module';
 
 @NgModule({
-  declarations: [FlightsPageComponent, PassengersPageComponent, SummaryPageComponent],
-  imports: [CommonModule, BookingRoutingModule],
+  declarations: [
+    FlightsPageComponent,
+    PassengersPageComponent,
+    SummaryPageComponent,
+    EditFlightComponent,
+  ],
+  imports: [CommonModule, BookingRoutingModule, MaterialDesignModule],
 })
 export class BookingModule {}
