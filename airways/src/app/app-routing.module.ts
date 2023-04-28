bfa3393885ac9f4829bf59cff10ba1104798d466
George <tsintsadze7@gmail.com>
2023-04-28 13:14:37 +0400

    feat: add home route

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
   {
     path: 'cart',
     loadChildren: () =>
