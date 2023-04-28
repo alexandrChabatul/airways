aae395bbf1a88b5f2cd6c928addfb9405c921b80
George <tsintsadze7@gmail.com>
2023-04-28 19:28:54 +0400

    fix: remove error state matcher from login page

--- airways/src/app/lazy-loaded-modules/auth/pages/login-page/login-page.component.ts
+++ airways/src/app/lazy-loaded-modules/auth/pages/login-page/login-page.component.ts
@@ -1,28 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 @Component({
   selector: 'airways-login-page',
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent implements OnInit {
   loginForm!: FormGroup;
-
-  matcher = new MyErrorStateMatcher();
 
   hide = true;
 
