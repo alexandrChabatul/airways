3dac1b528fa16e74cd9b5344de3ce2b687e1e892
sashkill94 <sashkill94@gmail.com>
2023-05-11 11:15:53 +0300

    feat" add cart-row component

---
+++ airways/src/app/lazy-loaded-modules/shopping-cart/components/cart-row/cart-row.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'airways-cart-row',
+  templateUrl: './cart-row.component.html',
+})
+export class CartRowComponent {
+
+}
--- airways/src/app/lazy-loaded-modules/shopping-cart/shopping-cart.module.ts
+++ airways/src/app/lazy-loaded-modules/shopping-cart/shopping-cart.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
+import { CartRowComponent } from './components/cart-row/cart-row.component';
 
 @NgModule({
-  declarations: [ShoppingCartPageComponent],
+  declarations: [ShoppingCartPageComponent, CartRowComponent],
   imports: [CommonModule, ShoppingCartRoutingModule],
 })
 export class ShoppingCartModule {}
