c999fd3ab04ba6ba6a2eb65988bac50ac9138c22
George <tsintsadze7@gmail.com>
2023-05-15 03:40:01 +0400

    feat: implement basics

--- airways/src/app/lazy-loaded-modules/booking/pages/passengers-page/passengers-page.component.ts
+++ airways/src/app/lazy-loaded-modules/booking/pages/passengers-page/passengers-page.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'airways-passengers-page',
   templateUrl: './passengers-page.component.html',
-  styleUrls: ['./passengers-page.component.scss'],
 })
 export class PassengersPageComponent {}
