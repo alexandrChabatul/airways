80a2e8bbdcb8b4125a9ed9cf368b2c9f8cca93dd
Nastya <himeriya97@mgmail.com>
2023-05-16 17:53:11 +0300

    feat: add components templates

---
+++ airways/src/app/lazy-loaded-modules/booking/components/payment-summary/payment-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'airways-payment-summary',
+  templateUrl: './payment-summary.component.html',
+  styleUrls: ['./payment-summary.component.scss']
+})
+export class PaymentSummaryComponent {
+
+}
--- airways/src/app/lazy-loaded-modules/booking/pages/summary-page/summary-page.component.ts
+++ airways/src/app/lazy-loaded-modules/booking/pages/summary-page/summary-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SvgIconService } from '../../../../core/services/svg-icon.service';
 
 @Component({
   selector: 'airways-summary-page',
   templateUrl: './summary-page.component.html',
   styleUrls: ['./summary-page.component.scss'],
 })
-export class SummaryPageComponent {}
+export class SummaryPageComponent {
+  constructor(private iconsService: SvgIconService) {
+    this.iconsService.addSvgIcon('summary');
+  }
+}
