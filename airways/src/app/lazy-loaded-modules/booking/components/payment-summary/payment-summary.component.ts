6bfa29f415b7c47d151f50f75bc1b9909ef0a77b
Nastya <himeriya97@mgmail.com>
2023-05-18 16:43:37 +0300

    feat: add summary page styles

--- airways/src/app/lazy-loaded-modules/booking/components/payment-summary/payment-summary.component.ts
+++ airways/src/app/lazy-loaded-modules/booking/components/payment-summary/payment-summary.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CurrencyFormatType } from '../../../../core/models/formats.models';
+import { selectCurrencyFormat } from '../../../../core/store/selectors/formats.selectors';
 
 @Component({
   selector: 'airways-payment-summary',
   templateUrl: './payment-summary.component.html',
-  styleUrls: ['./payment-summary.component.scss']
+  styleUrls: ['./payment-summary.component.scss'],
 })
-export class PaymentSummaryComponent {
+export class PaymentSummaryComponent implements OnInit {
+  public currency$!: Observable<CurrencyFormatType>;
 
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.currency$ = this.store.select(selectCurrencyFormat);
+  }
 }
--- airways/src/app/lazy-loaded-modules/booking/pages/summary-page/summary-page.component.ts
+++ airways/src/app/lazy-loaded-modules/booking/pages/summary-page/summary-page.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SvgIconService } from '../../../../core/services/svg-icon.service';
+import { Observable } from 'rxjs';
+import { CurrencyFormatType } from '../../../../core/models/formats.models';
+import { Store } from '@ngrx/store';
+import { selectCurrencyFormat } from '../../../../core/store/selectors/formats.selectors';
 
 @Component({
   selector: 'airways-summary-page',
   templateUrl: './summary-page.component.html',
   styleUrls: ['./summary-page.component.scss'],
 })
-export class SummaryPageComponent {
-  constructor(private iconsService: SvgIconService) {
+export class SummaryPageComponent implements OnInit {
+  public currency$!: Observable<CurrencyFormatType>;
+
+  constructor(private iconsService: SvgIconService, private store: Store) {
     this.iconsService.addSvgIcon('summary');
+  }
+
+  ngOnInit(): void {
+    this.currency$ = this.store.select(selectCurrencyFormat);
   }
 }
