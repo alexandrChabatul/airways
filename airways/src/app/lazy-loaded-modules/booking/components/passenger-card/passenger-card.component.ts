4db405aa172e1f2dd5588b1a4286044fdb28c4d0
Nastya <himeriya97@mgmail.com>
2023-05-20 16:26:18 +0300

    feat: add template for passenger card

---
+++ airways/src/app/lazy-loaded-modules/booking/components/passenger-card/passenger-card.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { SvgIconService } from '../../../../core/services/svg-icon.service';
+
+@Component({
+  selector: 'airways-passenger-card',
+  templateUrl: './passenger-card.component.html',
+  styleUrls: ['./passenger-card.component.scss'],
+})
+export class PassengerCardComponent implements OnInit {
+  public passengerForm!: FormGroup;
+
+  public maxDate: Date = new Date();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store,
+    private svgIconService: SvgIconService,
+  ) {
+    this.svgIconService.addSvgIcon('info');
+    this.svgIconService.addSvgIcon('assistance');
+  }
+
+  ngOnInit(): void {
+    this.passengerForm = this.formBuilder.group({
+      firstName: ['', { validators: [Validators.required, this.onlyLettersValidator] }],
+      lastName: ['', { validators: [Validators.required, this.onlyLettersValidator] }],
+      dateOfBirth: ['', { validators: [Validators.required] }],
+      gender: ['', { validators: [Validators.required] }],
+      needAssistance: '',
+      needBuggage: '',
+    });
+  }
+
+  private onlyLettersValidator(control: AbstractControl): ValidationErrors | null {
+    const onlyLettersRegex = /^[A-Za-z]+$/;
+    const isOnlyLetters = onlyLettersRegex.test(control.value);
+    return isOnlyLetters ? null : { isOnlyLetters: true };
+  }
+
+  get firstName() {
+    return this.passengerForm.controls['firstName'];
+  }
+
+  get lastName() {
+    return this.passengerForm.controls['lastName'];
+  }
+
+  get dateOfBirth() {
+    return this.passengerForm.controls['dateOfBirth'];
+  }
+
+  get gender() {
+    return this.passengerForm.controls['gender'];
+  }
+}
